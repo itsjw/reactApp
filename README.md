ffbf598a7b3d8a57368eba8381e0d46440b08b60
Your Name <you@example.com>
2017-10-28 12:27:37 +0100

    Initial commit from react-kickstart

---
+++ README.md
@@ -0,0 +1,85 @@
+# react-kickstart
+
+> just another react + webpack boilerplate
+
+<a target='_blank' rel='nofollow' href='https://app.codesponsor.io/link/aF8QWaPs78PSySxWPHXXNThZ/vesparny/react-kickstart'>
+  <img alt='Sponsor' width='888' height='68' src='https://app.codesponsor.io/embed/aF8QWaPs78PSySxWPHXXNThZ/vesparny/react-kickstart.svg' />
+</a>
+
+[![Build Status](https://travis-ci.org/vesparny/react-kickstart.svg)](https://travis-ci.org/vesparny/react-kickstart)
+[![Dependency status](https://david-dm.org/vesparny/react-kickstart/status.svg)](https://david-dm.org/vesparny/react-kickstart "Dependency status")
+[![Dev dependency status](https://david-dm.org/vesparny/react-kickstart/dev-status.svg)](https://david-dm.org/vesparny/react-kickstart#info=devDependencies "Dev dependency status")
+[![JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+See a [working demo](http://vesparny.github.io/react-kickstart/)
+
+## Features
+* webpack + vanilla hot-module-replacement
+* styling with [glamorous](https://github.com/paypal/glamorous)
+* prettier and standardjs
+* basic routing setup with react-router (you most likely need routing in your app)
+* unit tests ([jest](http://facebook.github.io/jest/) and [enzyme](http://airbnb.io/enzyme))
+* development and production build
+
+## Usage
+
+**Clone this repository**
+```
+git clone https://github.com/vesparny/react-kickstart.git
+```
+
+**Install**
+```
+npm install (or yarn install)
+```
+
+**Start the application in dev mode with hot-module-replacement**
+```
+npm run dev
+```
+
+**Run tests**
+```
+npm test
+```
+
+**Build for production**
+```
+npm run build
+```
+
+**Run production version**
+```
+npm run start
+```
+
+**RUN in production as a docker container**
+
+**Create docker image**
+
+```bash
+docker build -t react-kickstart .
+```
+
+**RUN using docker-compose.yml**
+
+```bash
+docker-compose up -d
+```
+
+**Lint with standard**
+```
+npm run lint
+```
+
+## Contributing
+
+PR and issues reporting are always welcome :)
+
+## License
+
+See LICENSE file
+
+## Changelog
+
+See CHANGELOG.md file
---
+++ src/components/Home.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import { Box, Flex } from 'reflexbox'
+import glamorous from 'glamorous'
+import Counter from '../components/Counter'
+import {
+  version,
+  dependencies,
+  homepage,
+  devDependencies
+} from '../../package.json'
+
+const Wrapper = glamorous(Box)(
+  {
+    textAlign: 'center'
+  },
+  (props, theme) => ({ color: theme.colors.black })
+)
+
+const Column = glamorous(Box)({}, (props, theme) => ({
+  color: theme.colors.grey
+}))
+
+const Container = glamorous(Flex)({
+  width: '60%',
+  margin: '0 auto'
+})
+
+const Ul = glamorous(Box)({
+  listStyle: 'none'
+})
+
+class Home extends Component {
+  state = {
+    counter: 0
+  }
+  increment () {
+    this.setState((prevState, props) => {
+      const count = (prevState.counter += 1)
+      return {
+        counter: count
+      }
+    })
+  }
+
+  render () {
+    const { counter } = this.state
+    const deps = Object.keys(dependencies).map((dep, i) => (
+      <li key={i}><b>{dep}</b> : {dependencies[dep]}</li>
+    ))
+    const devDeps = Object.keys(devDependencies).map((dep, i) => (
+      <li key={i + 10}><b>{dep}</b> : {devDependencies[dep]}</li>
+    ))
+
+    return (
+      <Wrapper>
+        <div>
+          <h1>
+            <a href={homepage}>react-kickstart</a>
+          </h1>
+          <h2>version {version}</h2>
+          With  💙 by <a href='https://twitter.com/vesparny'>@vesparny</a>
+          <p>
+            <a
+              href='https://david-dm.org/vesparny/react-kickstart'
+              title='Dependency status'
+            >
+              <img
+                alt='Dependency status'
+                src='https://david-dm.org/vesparny/react-kickstart/status.svg'
+              />
+            </a>
+            <a
+              href='https://david-dm.org/vesparny/react-kickstart#info=devDependencies'
+              title='Dev dependency status'
+            >
+              <img
+                alt='Dev dependency status'
+                src='https://david-dm.org/vesparny/react-kickstart/dev-status.svg'
+              />
+            </a>
+          </p>
+          <Counter count={counter} onIncrement={() => this.increment()} />
+          <h3>Powered by:</h3>
+          <Container wrap>
+            <Column sm={12} md={6}>
+              <h4> DEPENDENCIES:</h4>
+              <Ul>{deps}</Ul>
+            </Column>
+            <Column sm={12} md={6}>
+              <h4> DEV-DEPENDENCIES:</h4>
+              <Ul>{devDeps}</Ul>
+            </Column>
+          </Container>
+        </div>
+      </Wrapper>
+    )
+  }
+}
+
+export default Home
